87412c2c6b5a4b6411368f8b2d8402806c224003
Leonardo Nascimento <leonardofernandespro@gmail.com>
2025-07-02 14:41:09 -0300

    Adicionado as opções da lista de tarefa

--- index.py
+++ index.py
@@ -1,2 +1,9 @@
+"""
+1 - adicionar, ver, atualizar, completar, deletar completadas, sair
+"""
+
 def list():
-    return 'hello world!'
+    while True:
+        valor = input(f"{'='*30} \nLISTA DE TAREFAS \n {'='*30} Escolha uma opção abaixo: \n [ 1 ] - Adicionar tarefa \n [ 2 ] - Visualizar tarefas \n [ 3 ] - Atualizar tarefa \n [ 4 ] - Deletar tarefas concluídas \n [ 5 ] Sair")
+
+        
